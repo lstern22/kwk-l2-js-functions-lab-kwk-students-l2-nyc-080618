ef65c43c38ef63ce3072b2f0ac12efddaba35ea0
lstern22 <lstern220@gmail.com>
2018-08-08 19:40:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(myBlock){
+ if (myBlock > 42){
+   var block = myBlock - 42;
+ }
+else if (myBlock< 42) {
+  block = 42 - myBlock;
+  
+}
+ return block;
+ 
+ }
+ 
+function distanceFromHqInFeet (block1,block2) {
+   if (block1 > block2){
+   var feet = (myFeet - 42) * 264;
+  // var X = feet * 264;
+ }
+else if (myFeet< 42) {
+  feet = (42 - myFeet) * 264;
+  
+}
+return feet;
+}
+
+function distanceTravelledInFeet (myFeets) {
+   if (myFeets > 42){
+   var feets = (myFeets - 42) * 264;
+ }
+else if (myFeets< 42) {
+  feets = (42 - myFeets) * 264;
+  
+}
+return feets;
+}
+
+
